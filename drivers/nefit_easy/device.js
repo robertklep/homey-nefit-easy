2c574bed14c7a1f6db0c2fb684511fa44a72f712
Robert Klep <robert@klep.name>
2017-10-28 11:54:41 +0200

    More capabilities and added mobile card

--- drivers/nefit_easy/device.js
+++ drivers/nefit_easy/device.js
@@ -7,9 +7,11 @@ const DEBOUNCE_RATE = 500;
 const formatValue   = t => Math.round(t.toFixed(1) * 10) / 10
 
 // Capabilities
-const INDOOR_TEMP = 'measure_temperature';
-const TARGET_TEMP = 'target_temperature';
-const PRESSURE    = 'measure_pressure';
+const INDOOR_TEMP     = 'measure_temperature';
+const TARGET_TEMP     = 'target_temperature';
+const PRESSURE        = 'measure_pressure';
+const CLOCK_PROGRAMME = 'clock_programme';
+const OPERATING_MODE  = 'operating_mode';
 
 module.exports = class NefitEasyDevice extends Homey.Device {
 
@@ -29,7 +31,8 @@ module.exports = class NefitEasyDevice extends Homey.Device {
     await this.setAvailable();
 
     // Register capabilities.
-    this.registerMultipleCapabilityListener([ TARGET_TEMP ] , this.onSetTargetTemperature.bind(this), DEBOUNCE_RATE);
+    this.registerMultipleCapabilityListener([ TARGET_TEMP ],     this.onSetTargetTemperature.bind(this), DEBOUNCE_RATE);
+    this.registerMultipleCapabilityListener([ CLOCK_PROGRAMME ], this.onSetClockProgramme   .bind(this), DEBOUNCE_RATE);
 
     // Start syncing periodically..
     this.shouldSync = true;
@@ -47,30 +50,62 @@ module.exports = class NefitEasyDevice extends Homey.Device {
     return client;
   }
 
+  async setValue(cap, value) {
+    if (value == null) return;
+    if (typeof value === 'number') {
+      value = formatValue(value);
+    }
+    if (this.getCapabilityValue(cap) !== value) {
+      return await this.setCapabilityValue(cap, value);
+    }
+  }
+
   async updateStatus() {
     let [ status, pressure ] = await Promise.all([ this.client.status(), this.client.pressure() ]);
 
-    // Set measured temperature and pressure.
-    await this.setCapabilityValue(INDOOR_TEMP, formatValue(status['in house temp']));
-    if (this.hasCapability('measure_pressure') && pressure) {
-      await this.setCapabilityValue(PRESSURE, formatValue(pressure.pressure));
+    // Set modes and temperatures.
+    if (status) {
+      // Target temperature depends on the user mode: manual or program.
+      let temp = Number(status[ status['user mode'] === 'manual' ? 'temp manual setpoint' : 'temp setpoint' ])
+      await Promise.all([
+        this.setValue(CLOCK_PROGRAMME, status['user mode'] === 'clock'),
+        this.setValue(OPERATING_MODE,  status['boiler indicator']),
+        this.setValue(INDOOR_TEMP,     status['in house temp']),
+        this.setValue(TARGET_TEMP,     temp)
+      ]);
     }
 
-    // Target temperature depends on the user mode: manual or program.
-    let temp = Number(status[ status['user mode'] === 'manual' ? 'temp manual setpoint' : 'temp setpoint' ])
-    await this.setCapabilityValue(TARGET_TEMP, formatValue(temp));
+    // Set pressure, if the device supports it.
+    if (this.hasCapability('measure_pressure') && pressure) {
+      await this.setValue(PRESSURE, pressure.pressure);
+    }
   }
 
   onSetTargetTemperature(value, opts) {
+    this.log('setting target temperature to', value);
     if (this.getCapabilityValue(TARGET_TEMP) === value) {
       this.log('value matches current, not updating', value);
       return Promise.resolve();
     }
-    this.log('setting target temperature to', value);
     return this.client.setTemperature(value).then(s => {
       this.log('...status:', s.status);
       if (s.status === 'ok') {
-        return this.setCapabilityValue(TARGET_TEMP, value);
+        return this.setValue(TARGET_TEMP, value);
+      }
+    });
+  }
+
+  onSetClockProgramme(value, opts) {
+    value = value[CLOCK_PROGRAMME];
+    this.log('setting programme mode to', value ? 'clock' : 'manual');
+    if (this.getCapabilityValue(CLOCK_PROGRAMME) === value) {
+      this.log('value matches current, not updating');
+      return Promise.resolve();
+    }
+    return this.client.setUserMode(value ? 'clock' : 'manual').then(s => {
+      this.log('...status:', s.status);
+      if (s.status === 'ok') {
+        return this.setValue(CLOCK_PROGRAMME, value);
       }
     });
   }
